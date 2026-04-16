46a1ef6995de76e6181fbf12b408132b4a8edbce
moulidharan112006 <moulidharank11@gmail.com>
2026-04-16 18:08:48 +0530

    Initial commit

---
+++ LFSs/src/SumInRange.java
@@ -0,0 +1,14 @@
+public class SumInRange {
+    static int findSum(int[] arr,int l,int r){
+        int sum=0;
+        for(int i=l;i<=r;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 0, 1, 6, 7};
+        int l = 2,r = 5;
+        System.out.println(findSum(arr,l,r));
+    }
+}
