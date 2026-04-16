eba00d992e9a3ea07a68b9e5b477e43264bca46d
moulidharan112006 <moulidharank11@gmail.com>
2026-04-16 17:56:19 +0530

    Initial commit

---
+++ LFSs/src/PrefixSumArray.java
@@ -0,0 +1,12 @@
+public class PrefixSumArray {
+    static void prefixSum(int[] arr){
+        for(int i=1;i< arr.length;i++){
+            arr[i] = arr[i] + arr[i-1];
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 10, 5, 15};
+        prefixSum(arr);
+        for(int i=0;i< arr.length;i++) System.out.print(arr[i]+" ");
+    }
+}
