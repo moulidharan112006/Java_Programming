b36bbf268293e3f1aeee388c4c7320c2e60bdd43
moulidharan112006 <moulidharank11@gmail.com>
2026-04-17 21:32:58 +0530

    Initial commit

---
+++ LFSs/src/CountSubArrayWithTarget.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+public class CountSubArrayWithTarget {
+
+    //Brute Force
+    static int countSubarray(int[] arr,int k){
+        int count =0;
+        for(int i=0;i<arr.length;i++){
+            int sum =arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                sum+=arr[j];
+                if(sum == k) count++;
+            }
+        }
+        return count;
+    }
+
+    //Better
+    static int bCountSubarray(int[] arr,int k){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum=0,count =0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(sum == k) count++;
+            if(map.containsKey(sum - k)){
+                count+=map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 2, -2, -20, 10};
+        int k = -10;
+        System.out.println(countSubarray(arr,k));
+        System.out.println(bCountSubarray(arr,k));
+    }
+}
