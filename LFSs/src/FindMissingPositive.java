7fd2de47ddce10a0ab0b8af4499a18bddb9c3644
moulidharan112006 <moulidharank11@gmail.com>
2026-04-20 18:24:29 +0530

    Initial commit

---
+++ LFSs/src/FindMissingPositive.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class FindMissingPositive {
+    public static int firstMissingPositive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i: nums) set.add(i);
+
+        int i=1;
+        while(true){
+            if(!set.contains(i)){
+                return i;
+            }
+            i++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,4,-1,1};
+        System.out.println(firstMissingPositive(arr));
+    }
+}
