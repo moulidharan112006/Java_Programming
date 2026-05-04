9e08708ce85dc270fe43190affda023f57c4470a
moulidharan112006 <moulidharank11@gmail.com>
2026-05-04 20:53:00 +0530

    Initial commit

---
+++ LFSs/src/PlusOne.java
@@ -0,0 +1,19 @@
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        int[] res = plusOne(arr);
+        for(int i : res) System.out.print(i+" ");
+    }
+}
