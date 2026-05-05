247bc2a673d971a96a8d59218508a4182c7c521c
moulidharan112006 <moulidharank11@gmail.com>
2026-05-05 21:19:39 +0530

    Initial commit

---
+++ LFSs/src/MaximumGap.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class MaximumGap {
+    static int maxDiff(int[] arr) {
+        Arrays.sort(arr);
+        int maxGap =0;
+        for(int i=1;i<arr.length;i++){
+            maxGap = Math.max(maxGap,arr[i] - arr[i-1]);
+        }
+        return maxGap;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,10,5};
+        System.out.println(maxDiff(arr));
+    }
+}
