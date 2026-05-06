8cfefa51bee228cbf0ce3cfacd9588d0cb038862
moulidharan112006 <moulidharank11@gmail.com>
2026-05-06 10:43:28 +0530

    Initial commit

---
+++ LFSs/src/ProductOfArrayExpectItself.java
@@ -0,0 +1,25 @@
+public class ProductOfArrayExpectItself {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+        int[] sufix = new int[n];
+        prefix[0] = 1;
+        sufix[n-1] = 1;
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+        for(int i=n-2;i>=0;i--){
+            sufix[i] = sufix[i + 1] * nums[i + 1];
+        }
+        int[] res = new int[nums.length];
+        for(int i=0;i<n;i++){
+            res[i] = sufix[i] * prefix[i];
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int[] res = productExceptSelf(arr);
+        for(int i=0;i<res.length;i++) System.out.print(res[i]+" ");
+    }
+}
